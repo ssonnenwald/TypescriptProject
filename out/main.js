addea05e30b4f5526b506031d87f109d1934c290
Scott R. Sonnenwald <ssonnenwald@e2g.com>
2024-08-20 22:14:08 -0400

    Update

--- out/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const derived_1 = require("./classes/derived");
-var derived = new derived_1.Derived();
-//# sourceMappingURL=main.js.map
